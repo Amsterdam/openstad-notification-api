cc73e3e7721159d339cdc0d5754b58090c14b2af
mitkoevoets <mitkoevoets@vaerens.com>
2020-05-10 22:05:37 +0200

    Cleanup

--- src/app.js
+++ src/app.js
@@ -3,11 +3,8 @@ import 'babel-polyfill';
 import {appConfig} from './config/app';
 import app from './bootstrap/express';
 
-if (!module.parent) {
-  // listen on port config.port
-  app.listen(appConfig.port, () => {
-    console.info(`server started on port ${appConfig.port}`);
-  });
-}
+app.listen(appConfig.port, () => {
+  console.info(`server started on port ${appConfig.port}`);
+});
 
 export default app;
