0e35970ba2a4d2113966228fad0d93ec22759125
mitkoevoets <mitkoevoets@vaerens.com>
2020-05-17 17:23:32 +0200

    Cleanup

--- src/tests/ruleset.test.js
+++ src/tests/ruleset.test.js
@@ -1,9 +1,9 @@
 import rulesetService from '../services/ruleset.service';
-const newIdeaFeedbackToUser = require('../db/factories/json/newIdeaFeedbackToUser.json')
+
+const newIdeaFeedbackToUser = require('../db/factories/json/newIdeaFeedbackToUser.json');
 
 test('TEST: ruleset.matches method rejects rulesets which do not match the data', () => {
-  const data = {
-  };
+  const data = {};
 
   expect(rulesetService.matches(JSON.stringify(newIdeaFeedbackToUser), {})).toBe(false);
   expect(rulesetService.matches(JSON.stringify(newIdeaFeedbackToUser), data)).toBe(false);
@@ -12,7 +12,15 @@ test('TEST: ruleset.matches method rejects rulesets which do not match the data'
 
 test('TEST: ruleset.matches method approves rulesets which match the data', () => {
   const data = {
-
+    eventType: 'CREATE',
+    clientKey: 'gem_amstelveen_#@EWDs',
+    userRole: 'moderator',
+    userEmail: 'mitkoevoets@car.bl',
+    resource: 'idea',
+  };
+  const ruleset = {
+    body: JSON.stringify(newIdeaFeedbackToUser),
   }
-  expect(rulesetService.matches(JSON.stringify(newIdeaFeedbackToUser), data)).toBe(true);
+
+  expect(rulesetService.matches(ruleset, data)).toBe(true);
 });
