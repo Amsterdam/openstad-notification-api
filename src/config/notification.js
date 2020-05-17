73beb644bb1043dc6d904e6fb0294930e6850d91
mitkoevoets <mitkoevoets@vaerens.com>
2020-05-17 13:42:22 +0200

    Setup Ethereal mail

--- src/config/notification.js
+++ src/config/notification.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 
 export default {
   smpt: {
-    host: "smtp.ethereal.email",
+    host: 'smtp.ethereal.email',
     port: 587,
-    secure: false, // true for 465, false for other ports
     auth: {
-      user: 'testAccount.user', // generated ethereal user
-      pass: 'testAccount.pass', // generated ethereal password
+      user: 'roderick.ziemann70@ethereal.email',
+      pass: 'Z5BW2YxFVgCEPkJa2h'
     },
+    secure: false, // true for 465, false for other ports
   },
   fromAddress: process.env.MAIL_FROM_ADDRESS,
   testAddress: process.env.MAIL_TEST_EMAIL_ADDRESS,
